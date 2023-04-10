1826918781e7c5302135ba677171aceca25af886
bny <eymen_15@hotmail.com>
2023-04-10 17:41:00 +0300

    Release version 1.0.0 of UnrealEngine plugin blueprint demo project

---
+++ Source/HMSUEPlugin_BPDemo/HMSUEPlugin_BPDemo.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class HMSUEPlugin_BPDemo : ModuleRules
+{
+	public HMSUEPlugin_BPDemo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
